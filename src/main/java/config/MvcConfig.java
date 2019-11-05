68ba3c5dce029ea465d06dd9f5ae2d8a0c0a85be
ArseniyIFMO <vatrushka.96@mail.ru>
2019-11-05 13:42:06 +0300

    Logging added

---
+++ src/main/java/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/main").setViewName("main");
+        registry.addViewController("/users").setViewName("users");
+    }
+
+}
