5bd09ad14ad4032ed6bbc93044890b7dff90a315
ArseniyIFMO <vatrushka.96@mail.ru>
2019-12-20 19:17:49 +0300

    sorting updated

--- src/main/java/myPckg/UserRepo.java
+++ src/main/java/myPckg/UserRepo.java
@@ -1,6 +1,9 @@
 package myPckg;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
